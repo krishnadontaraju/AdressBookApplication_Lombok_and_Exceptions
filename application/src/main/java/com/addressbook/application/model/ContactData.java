a69dc58ebed377d58c0379abed83e8cc7e24900f
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-05-20 21:55:33 +0530

    UPDATED : controller and dto

--- application/src/main/java/com/addressbook/application/model/ContactData.java
+++ application/src/main/java/com/addressbook/application/model/ContactData.java
@@ -1,5 +1,44 @@
 package com.addressbook.application.model;
 
+import com.addressbook.application.dto.ContactDTO;
+
 public class ContactData {
 
+	private int contactId;
+	private String firstName;
+	private String lastName;
+	private long contactNumber;
+	public ContactData(int contactId, ContactDTO contactDTO) {
+		super();
+		this.contactId = contactId;
+		this.firstName = contactDTO.firstName;
+		this.lastName = contactDTO.lastName;
+		this.contactNumber = contactDTO.contactNumber;
+	}
+	public int getContactId() {
+		return contactId;
+	}
+	public void setContactId(int contactId) {
+		this.contactId = contactId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(long contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	
 }
