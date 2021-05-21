0de45109fdae4ec21baa81df8aeb76f7465027a7
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-05-21 12:23:11 +0530

    ADDED : sl4j and logging

--- application/src/main/java/com/addressbook/application/Application.java
+++ application/src/main/java/com/addressbook/application/Application.java
@@ -3,11 +3,16 @@ package com.addressbook.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+import sun.util.logging.resources.logging;
+
 @SpringBootApplication
+@Slf4j
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		log.info("Addrress Book Application Started");
 	}
 
 }
